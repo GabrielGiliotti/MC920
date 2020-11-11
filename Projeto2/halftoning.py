bb103a69420d9785d50fd2492480d8c44172c372
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2020-11-10 21:50:53 -0300

    Algoritmo Projeto 2 finalizado

--- Projeto2/halftoning.py
+++ Projeto2/halftoning.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+from cv2 import IMREAD_COLOR, BORDER_CONSTANT
+from math import floor
 from numpy import newaxis
 import numpy as np
 import sys
@@ -6,99 +8,180 @@ import sys
 floyd_steinberg_filter = 1/16*np.array([[0,0,7],
                                         [3,5,1]])
 
+stevenson_arce_filter = 1/200*np.array([[0,0,0,0,0,32,0],
+                                        [12,0,26,0,30,0,16],
+                                        [0,12,0,26,0,12,0],
+                                        [5,0,12,0,12,0,5]])
 
-def slide_filter(image, kernel, sub_factor, band):
-    lista=[] # lista de saida que se tornara a nova imagem g(x,y)
-    kernel_rows = len(kernel)
-    kernel_columns = len(kernel[0]) # preciso das dimensoes (numero de linhas e colunas) do kernel para aplicar os filtros corretamente
+burkes_filter = 1/32*np.array([[0,0,0,8,4],
+                               [2,4,8,4,2]])
+                        
+sierra_filter = 1/32*np.array([[0,0,0,5,3],
+                               [2,4,5,4,2],
+                               [0,2,3,2,0]])
 
-    rows = len(image)
-    columns = len(image[0])
+stucki_filter = 1/42*np.array([[0,0,0,8,4],
+                               [2,4,8,4,2],
+                               [1,2,4,2,1]])
 
-    for i in range(0,rows-sub_factor):
-        for j in range(1,columns-sub_factor):            
-            # image[i][j][b/g/r] eh o valor f(x,y) da mascara. Colocando o pixel f(x,y) no kernel para completar a mascara
-            last_value = -1
-            if(image[i][j][band] < 128):
-                lista.append(0)
-                #image[i][j][band] = 0
-                last_value = 0
-            else:
-                lista.append(1)
-                #image[i][j][band] = 255
-                last_value = 1
-            # Calculamos o erro para fazer a propagacao
-            erro = image[i][j][band] - (last_value * 255)
-            # pro dado recorte da iteracao corrente, fazemos com que os valores do recorte sejam acrescidos de kernel*erro
-            image[i:i+kernel_rows, j-sub_factor:j+kernel_columns-sub_factor,band] = image[i:i+kernel_rows, j-sub_factor:j+kernel_columns-sub_factor,band] + (kernel*erro)
+jarvis_judice_ninke_filter = 1/48*np.array([[0,0,0,7,5],
+                                            [3,5,7,5,3],
+                                            [1,3,5,3,1]])
 
-    return image
-    #return np.array(lista).reshape(512, 512)
+def slide_filter(image, kernel):
+
+    # column_x eh o numero de colunas com valor 0 que devemos adicionar nas laterais da imagem para aplicar os filtros 
+    column_x = floor(kernel.shape[1]/2)
+    # row_y eh o numero de linhas com valor 0 que devemos adicionar na base da imagem.
+    # Para esse projeto nao eh necessario inserir linha no topo da imagem
+    row_y = kernel.shape[0] - 1 
+   
+    image = cv.copyMakeBorder(image, top=0, bottom=row_y, left=column_x, right=column_x, borderType=BORDER_CONSTANT, value=0)
+    image = image.astype(float, copy=False)
+    
+    # X e Y sao os valores maximos do range que vamos percorrer na imagem de entrada com bordas
+    X = image.shape[1] - column_x
+    Y = image.shape[0] - row_y
+    
+    # img_out sera a imagem que recebera valores de saida
+    img_out = np.zeros((image.shape[0],image.shape[1]))
+
+    for j in range(Y):      
+        for i in range(column_x, X):
+            # Pegamos o maximo valor entre 0 e pixel corrente pois existem pixels com valores negativos
+            image[j,i] = max(0,image[j,i])
+            # Fazendo a divisao por 128, estamos separando os pixels com valores entre (0.,1.99)
+            # Fazendo floor, definimos se o valor sera 0 ou 255 (ou seja, 0 ou 1)
+            img_out[j,i] = 255 * floor(image[j,i]/128)
+            # Erro eh a diferenca de valor entre imagem original e imagem de saida
+            erro = image[j,i] - img_out[j,i]
+            # Pegamos o slice com as mesmas dimensoes do kernel
+            img_slice = image[j:j+row_y+1, i-column_x:i+column_x+1]
+            # Propagamos o erro para pedaco corrente da imagem
+            img_slice += (kernel*erro)
+    
+    # Removendo as bordas com valor 0 da imagem de saida
+    img_out = img_out[:Y,column_x:X]
+    # Retorna cada banda BGR
+    return img_out   
+
+
+def slide_filter_alternado(image, kernel):
+
+    # column_x eh o numero de colunas com valor 0 que devemos adicionar nas laterais da imagem para aplicar os filtros 
+    column_x = floor(kernel.shape[1]/2)
+    # row_y eh o numero de linhas com valor 0 que devemos adicionar na base da imagem.
+    # Para esse projeto nao eh necessario inserir linha no topo da imagem
+    row_y = kernel.shape[0] - 1 
+    # Armazenamos uma copia flipada do kernel para aplicar nas iteracoes alternadas
+    fliped_kernel = np.fliplr(kernel)
+   
+    image = cv.copyMakeBorder(image, top=0, bottom=row_y, left=column_x, right=column_x, borderType=BORDER_CONSTANT, value=0)
+    image = image.astype(float, copy=False)
+    
+    # X e Y sao os valores maximos do range que vamos percorrer na imagem de entrada com bordas
+    X = image.shape[1] - column_x
+    Y = image.shape[0] - row_y
+    
+    # img_out sera a imagem que recebera valores de saida
+    img_out = np.zeros((image.shape[0],image.shape[1]))
+
+    for j in range(Y): 
+        
+        # Reverse if odd indexed line
+        reverse = (j % 2 == 1)
+        current_kernel = fliped_kernel if reverse else kernel     
+        
+        for i in range(column_x, X)[::-1 if reverse else 1]:
+            image[j,i] = max(0,image[j,i])
+            img_out[j,i] = 255 * floor(image[j,i]/128)
+            # Calculos de difusao do Erro
+            erro = image[j,i] - img_out[j,i]
+            img_slice = image[j:j+row_y+1, i-column_x:i+column_x+1]
+            img_slice += (kernel*erro)
+    
+    # Removendo as bordas com valor 0 da imagem de saida
+    img_out = img_out[:Y,column_x:X]
+    # Retorna cada banda BGR
+    return img_out   
 
 
 
 def halftoning_alternado(imgPath, filtro):
-    print("nada ainda")
+    # Leitura da Img
+    img = cv.imread(imgPath, IMREAD_COLOR)
+    img = img.astype(float, copy=False)
+
+    if(filtro == "Floyd_Steinberg"):
+        # para cada Banda BGR da imagem, aplicamos o filtro
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter_alternado(img[:,:,i], floyd_steinberg_filter)        
+
+    elif(filtro == "Stevenson_Arce"):
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter_alternado(img[:,:,i], stevenson_arce_filter)
+
+    elif(filtro == "Burkes"):
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter_alternado(img[:,:,i], burkes_filter)
+
+    elif(filtro == "Sierra"):
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter_alternado(img[:,:,i], sierra_filter)
+
+    elif(filtro == "Stucki"):
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter_alternado(img[:,:,i], stucki_filter)    
+    
+    elif(filtro == "Jarvis_Judice_Ninke"):
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter_alternado(img[:,:,i], jarvis_judice_ninke_filter) 
+    else:
+        print("Filtro nao definido ou inexistente !")
+        return
+
+    index = imgPath.rfind("\\")
+    imgName = imgPath[index+1:]
+    cv.imwrite("Outputs/Alternate_" + filtro + "_" + imgName, img)
 
 
 def halftoning_direto(imgPath, filtro):
     # Leitura da Img
-    img = cv.imread(imgPath).astype(np.uint8)
-
-    # Separacao da Img orginal em BGR
-    b,g,r = cv.split(img)
-    k = np.zeros_like(b)
-    b = cv.merge([b,k,k])
-    g = cv.merge([k,g,k])
-    r = cv.merge([k,k,r])
-
+    img = cv.imread(imgPath, IMREAD_COLOR)
+    img = img.astype(float, copy=False)
 
     if(filtro == "Floyd_Steinberg"):
-        # top, bottom, left, right
-        # Acrescentando bordas para nas imagens BGR para aplicar filtro
-        for i in range(0,3): 
-            img_border = cv.copyMakeBorder(img, 0, 1, 1, 1, cv.BORDER_CONSTANT, None, 0)
-            img_masked = slide_filter(img_border, floyd_steinberg_filter, 1, i)
-        
-        b_img_border = cv.copyMakeBorder(b, 0, 1, 1, 1, cv.BORDER_CONSTANT, None, 0)
-        b_img_masked = slide_filter(b_img_border, floyd_steinberg_filter, 1, 0)
-        g_img_border = cv.copyMakeBorder(g, 0, 1, 1, 1, cv.BORDER_CONSTANT, None, 0)
-        g_img_masked = slide_filter(g_img_border, floyd_steinberg_filter, 1, 1)
-        r_img_border = cv.copyMakeBorder(r, 0, 1, 1, 1, cv.BORDER_CONSTANT, None, 0)
-        r_img_masked = slide_filter(r_img_border, floyd_steinberg_filter, 1, 2)
-
+        # para cada Banda BGR da imagem, aplicamos o filtro
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter(img[:,:,i], floyd_steinberg_filter)        
 
     elif(filtro == "Stevenson_Arce"):
-        print("Stevenson_Arce escolhido")
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter(img[:,:,i], stevenson_arce_filter)
+
     elif(filtro == "Burkes"):
-        print("Burkes escolhido")
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter(img[:,:,i], burkes_filter)
+
     elif(filtro == "Sierra"):
-        print("Sierra escolhido")
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter(img[:,:,i], sierra_filter)
+
     elif(filtro == "Stucki"):
-        print("Stucki escolhido")
-    elif(filtro == "Jarvis_Judice_Ninke"):
-        print("Jarvis_Judice_Ninke escolhido")
-    else:
-        print("Filtro nao definido !")
-        return
-    # Aplicar os filtros para cada espectro
-
-    # Algoritmo na aula 13, por volta do minuto 50
-    # No proprio algoritmo, eh calculado o erro.
-    # Apos calculado o erro, aplicamos a mascara multiplicada pelo erro 
-    # Aplicamos as mascaras em cada espectro
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter(img[:,:,i], stucki_filter)    
     
-    # finalemnte unimos os espctros em uma imagem de saida 
-    # OBS: Percorrer a imagem em 2 orientacoes
-    # Na ida mascara vai no sentido normal, na volta, flipar a mascara
-
+    elif(filtro == "Jarvis_Judice_Ninke"):
+        for i in range(img.shape[2]):
+            img[:,:,i] = slide_filter(img[:,:,i], jarvis_judice_ninke_filter) 
+    else:
+        print("Filtro nao definido ou inexistente !")
+        return
+   
     # Saida imgs
     index = imgPath.rfind("\\")
     imgName = imgPath[index+1:]
-    cv.imwrite("Outputs/"+ imgName +"_blue.png", b_img_masked) 
-    cv.imwrite("Outputs/"+ imgName +"_green.png",g_img_masked) 
-    cv.imwrite("Outputs/"+ imgName +"_red.png", r_img_masked) 
-    cv.imwrite("Outputs/"+ imgName +"_merged.png", img_masked)
+    cv.imwrite("Outputs/" + filtro + "_" + imgName, img)
 
 
 def main(argv1, argv2, argv3):
