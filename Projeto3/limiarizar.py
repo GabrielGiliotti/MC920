d4ed18d2e07349e197ab38543c6da63192acfd72
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2020-11-29 02:36:53 -0300

    Projeto3 quase pronto (faltam ajustes)

---
+++ Projeto3/limiarizar.py
@@ -0,0 +1,160 @@
+from cv2 import imwrite, imread
+from cv2 import imshow, waitKey, destroyAllWindows, IMREAD_GRAYSCALE
+from os.path import join, basename, splitext
+from math import exp
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Algoritmo de aplicacao de limiarizacao global ou local em uma imagem')
+    parser.add_argument('imgPath', help='path completo para o arquivo (.pgm)')
+    parser.add_argument('methodName', help='Nome de um metodo de limiarizacao escolhido: global, bernsen, niblack, sauvola, more, contrast, mean, median')
+    parser.add_argument('--thresh', help='Valor que pode ser definido para limiarizacao global. Usado como default valor 128', default=128, type=int)
+    parser.add_argument('--size', help='Tamanho da janela para limiarizacao local (default eh 7, para uma janela 7x7).', default=7, type=int)
+    parser.add_argument('--k', help='Valor que pode ser definido para o parametro "k". Valor default 0.25', default=0.25, type=float)
+    parser.add_argument('--r', help='Valor que pode ser definido para o parametro "r". Valor default 0.5', default=0.5, type=float)
+    parser.add_argument('--p', help='Valor que pode ser definido para o parametro "p". Valor default 2', default=2, type=float)
+    parser.add_argument('--q', help='Valor que pode ser definido para o parametro "q". Valor default 10.', default=10, type=float)
+    
+    args = parser.parse_args()
+    # leitura e copia da imagem
+    img = imread(args.imgPath, IMREAD_GRAYSCALE)
+    inputImg = img.copy()
+
+    # Aux para ver a imagem que esta sendo lida na entrada
+    show_image(inputImg, None)
+
+    if(args.methodName == "global"):
+        img = global_thresholding(img, args.thresh)
+        # Aux para ver a imagem que esta sendo enviada para saida
+        show_image(img, None)
+        # argumentos: img de saida, img original, salvar histograma, nome da img, pasta de saida
+        histograma_e_pixels(img, inputImg, save=True, name="global_"+basename(args.imgPath), folder="Outputs")
+        save_image(img, args.imgPath, "global", "Outputs")
+    else:
+        # argumentos: img de saida, metodo utilizado, tamanho janela, valor k, valor r, valor p, valor q
+        img = local_thresholding(img, args.methodName, args.size, args.k, args.r, args.p, args.q)
+        # Aux para ver a imagem que esta sendo enviada para saida
+        show_image(img, None)
+        # argumentos: img de saida, img original, salvar histograma, nome da img, pasta de saida
+        histograma_e_pixels(img, inputImg, save=True, name="local_"+ args.methodName +"_"+basename(args.imgPath), folder="Outputs")
+        save_image(img, args.imgPath, "local_"+ args.methodName, "Outputs")
+
+
+
+# Global: valor fixado em 128
+def global_thresholding(img, threshold_number):
+    return np.uint8(np.where(img > threshold_number, 255, 0))
+
+
+def local_thresholding(img, methodName, size, k, r, p, q):
+    
+    t_mat = np.zeros(img.shape)
+    deltaXY = size//2
+    
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            janela_metodo_local = img[max(0, y-deltaXY) : min(y+deltaXY+1, img.shape[0]),
+                                      max(0, x-deltaXY) : min(x+deltaXY+1, img.shape[1])]
+            
+            if(methodName == "bernsen"):
+                t_mat[y,x] = bernsen(janela_metodo_local, k, r, p, q)
+            elif(methodName == "niblack"):
+                t_mat[y,x] = niblack(janela_metodo_local, k, r, p, q)
+            elif(methodName == "sauvola"):
+                t_mat[y,x] = sauvola_pietaksinen(janela_metodo_local, k, r, p, q)
+            elif(methodName == "more"):
+                t_mat[y,x] = phansalskar_more_sabale(janela_metodo_local, k, r, p, q)
+            elif(methodName == "contrast"):
+                t_mat[y,x] = contrast_method(janela_metodo_local, k, r, p, q)
+            elif(methodName == "mean"):
+                t_mat[y,x] = mean_method(janela_metodo_local, k, r, p, q)
+            elif(methodName == "median"):
+                t_mat[y,x] = median_method(janela_metodo_local, k, r, p, q)
+            else:
+                print("Metodo nao encontrado !")
+                t_mat[y,x] = t_mat[y,x]
+            
+    img = np.uint8(np.where(img >= t_mat, 255, 0))
+    return img
+  
+
+# Metodo Local de thresholding: (zmax + zmin)/2 
+def bernsen(win, *_):
+    return (np.amax(win) - np.amin(win))/2
+
+# Metodo Local de thresholding: mean + k*stdev
+# Metodo estatistico onde k eh um parametro de ajuste.
+def niblack(win, k, *_):
+    return np.mean(win) + k*np.std(win)
+
+# Metodo Local de thresholding projetado sobre Niblack, para imagens com iluminacao ruim.
+# k=0.5 e r=128 sugeridos
+def sauvola_pietaksinen(win, k, r, *_):
+    return np.mean(win) * (1 + k*(np.std(win)/r - 1))
+
+# Metodo Local de thresholding projetado sobre Sauvola e Pietaksinen, para imgens de baixo contraste.
+# k=0.25, r=0.5, p=2 and q=10 sao valores sugeridos.
+def phansalskar_more_sabale(win, k, r, p, q):
+    mean = np.mean(win)
+    return mean * (1 + p * exp(-1*q*mean) + k * (np.std(win)/r - 1))
+
+# Metodo Local de thresholding para checagem de contraste.
+def contrast_method(win, *_):
+    shape = win.shape
+    pixel = win[shape[0]//2, shape[1]//2]
+    return 0 if abs(int(pixel)-int(np.amax(win))) < abs(int(pixel)-int(np.amin(win))) else 255
+
+# Metodo Local de thresholding: mean
+def mean_method(win, *_):
+    return np.mean(win)
+
+# Metodo Local de thresholding: median
+def median_method(win, *_):
+    return np.median(win)
+
+# Metodo auxiliar para verificar imagem de entrada e saida
+def show_image(img, name='Image'):
+    imshow(name, img)
+    waitKey(0)
+    destroyAllWindows()
+
+
+def histograma_e_pixels(img, inputImg, save=True, name='image', folder='Outputs'):
+    
+    # Plot do histograma imagem original.
+    plt.hist(inputImg, bins='auto')
+    plt.title("Histograma " + name + " Input")
+    plt.xlim(0,255)
+    plt.xlabel("Niveis de Cinza")
+    plt.ylabel("Quantidade de Pixels")
+
+    # Salva histograma da img como um arquivo de saida
+    if save:
+        f,_ = splitext(name)
+        name = "histograma_" + f + '.png'
+        path = join(folder,name)
+        plt.savefig(path)
+    
+    # Retorna proporcoes de pixels brancos e pretos para imagem de saida
+    qtd_pixels = img.size
+    brancos = np.count_nonzero(img)
+    pretos = qtd_pixels - brancos
+    print()
+    print("Fracao de pixels brancos: ", brancos/qtd_pixels)
+    print("Fracao de pixels pretos: ", pretos/qtd_pixels)
+
+
+def save_image(img, path, method, folder):
+    name = method.lower() + '_' + basename(path)
+    # Se desejar a saida em .pgm, comente a duas linhas indicadas a seguir
+    name = name[:-4] # comente aqui
+    name = name + ".png" # comente aqui
+    imwrite(join(folder, name), img)
+
+
+if __name__ == '__main__':  
+    main()
+    
